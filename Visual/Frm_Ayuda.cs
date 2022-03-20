cf3824d3ac7d2334f7a31bd2cd52928442be51f5
hlbv17 <helenlbv17@gmail.com>
2022-03-20 18:53:35 -0500

    ventana ayuda y editar peticiones

---
+++ Visual/Frm_Ayuda.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Visual
+{
+    public partial class Frm_Ayuda : Form
+    {
+        Btn_Comportamiento cbtn = new Btn_Comportamiento();
+
+        public Frm_Ayuda()
+        {
+            InitializeComponent();
+        }
+
+        private void Frm_Ayuda_Load(object sender, EventArgs e)
+        {
+            this.pncontenido.BackColor = Color.FromArgb(200, 255, 255, 255);
+        }
+    }
+}
---
+++ Visual/Frm_Peticion_Editar.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Visual
+{
+    public partial class Frm_Peticion_Editar : Form
+    {
+        Btn_Comportamiento cbtn = new Btn_Comportamiento();
+        public Frm_Peticion_Editar()
+        {
+            InitializeComponent();
+        }
+
+        private void Frm_Peticion_Editar_Load(object sender, EventArgs e)
+        {
+            this.pncontenido.BackColor = Color.FromArgb(200, 255, 255, 255);
+        }
+    }
+}
