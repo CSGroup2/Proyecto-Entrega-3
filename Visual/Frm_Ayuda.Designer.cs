b617996b45f2fa4df2d8a2359bf614fb797cd8a5
hlbv17 <helenlbv17@gmail.com>
2022-03-21 16:14:09 -0500

    ...

--- Visual/Frm_Ayuda.Designer.cs
+++ Visual/Frm_Ayuda.Designer.cs
@@ -32,24 +32,10 @@ namespace Visual
             this.panel1 = new System.Windows.Forms.Panel();
             this.label2 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.pncontenido = new System.Windows.Forms.Panel();
-            this.btn11 = new System.Windows.Forms.Button();
-            this.btn10 = new System.Windows.Forms.Button();
-            this.btn9 = new System.Windows.Forms.Button();
-            this.btn8 = new System.Windows.Forms.Button();
-            this.btn7 = new System.Windows.Forms.Button();
-            this.btn6 = new System.Windows.Forms.Button();
-            this.btn5 = new System.Windows.Forms.Button();
-            this.btn4 = new System.Windows.Forms.Button();
-            this.btn3 = new System.Windows.Forms.Button();
-            this.btn2 = new System.Windows.Forms.Button();
-            this.btn1 = new System.Windows.Forms.Button();
-            this.btn12 = new System.Windows.Forms.Button();
-            this.btn13 = new System.Windows.Forms.Button();
-            this.btn14 = new System.Windows.Forms.Button();
+            this.btnAyuda = new System.Windows.Forms.Button();
+            this.btnConfig = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
-            this.pncontenido.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -70,7 +56,7 @@ namespace Visual
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Century Gothic", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(54)))), ((int)(((byte)(75)))));
-            this.label2.Location = new System.Drawing.Point(248, 9);
+            this.label2.Location = new System.Drawing.Point(237, 9);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(137, 44);
             this.label2.TabIndex = 1;
@@ -81,189 +67,32 @@ namespace Visual
             this.panel2.BackColor = System.Drawing.Color.LightGray;
             this.panel2.BackgroundImage = global::Visual.Properties.Resources._9;
             this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.panel2.Controls.Add(this.pncontenido);
+            this.panel2.Controls.Add(this.btnConfig);
+            this.panel2.Controls.Add(this.btnAyuda);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel2.Location = new System.Drawing.Point(0, 63);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(655, 387);
             this.panel2.TabIndex = 6;
             // 
-            // pncontenido
+            // btnAyuda
             // 
-            this.pncontenido.AutoScroll = true;
-            this.pncontenido.BackColor = System.Drawing.Color.White;
-            this.pncontenido.Controls.Add(this.btn14);
-            this.pncontenido.Controls.Add(this.btn13);
-            this.pncontenido.Controls.Add(this.btn12);
-            this.pncontenido.Controls.Add(this.btn11);
-            this.pncontenido.Controls.Add(this.btn10);
-            this.pncontenido.Controls.Add(this.btn9);
-            this.pncontenido.Controls.Add(this.btn8);
-            this.pncontenido.Controls.Add(this.btn7);
-            this.pncontenido.Controls.Add(this.btn6);
-            this.pncontenido.Controls.Add(this.btn5);
-            this.pncontenido.Controls.Add(this.btn4);
-            this.pncontenido.Controls.Add(this.btn3);
-            this.pncontenido.Controls.Add(this.btn2);
-            this.pncontenido.Controls.Add(this.btn1);
-            this.pncontenido.Location = new System.Drawing.Point(21, 17);
-            this.pncontenido.Name = "pncontenido";
-            this.pncontenido.Size = new System.Drawing.Size(607, 344);
-            this.pncontenido.TabIndex = 14;
+            this.btnAyuda.Location = new System.Drawing.Point(218, 203);
+            this.btnAyuda.Name = "btnAyuda";
+            this.btnAyuda.Size = new System.Drawing.Size(183, 105);
+            this.btnAyuda.TabIndex = 0;
+            this.btnAyuda.Text = "Manual de Usuario";
+            this.btnAyuda.UseVisualStyleBackColor = true;
+            this.btnAyuda.Click += new System.EventHandler(this.btnAyuda_Click);
             // 
-            // btn11
+            // btnConfig
             // 
-            this.btn11.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btn11.Location = new System.Drawing.Point(0, 530);
-            this.btn11.Name = "btn11";
-            this.btn11.Size = new System.Drawing.Size(590, 53);
-            this.btn11.TabIndex = 10;
-            this.btn11.Text = "¿Cómo asignar ambulancia-conductor en SGAR?";
-            this.btn11.UseVisualStyleBackColor = true;
-            this.btn11.Click += new System.EventHandler(this.btn11_Click);
-            // 
-            // btn10
-            // 
-            this.btn10.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btn10.Location = new System.Drawing.Point(0, 477);
-            this.btn10.Name = "btn10";
-            this.btn10.Size = new System.Drawing.Size(590, 53);
-            this.btn10.TabIndex = 9;
-            this.btn10.Text = "¿Cómo modificar los datos de las peticiones en SGAR?";
-            this.btn10.UseVisualStyleBackColor = true;
-            this.btn10.Click += new System.EventHandler(this.btn10_Click);
-            // 
-            // btn9
-            // 
-            this.btn9.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btn9.Location = new System.Drawing.Point(0, 424);
-            this.btn9.Name = "btn9";
-            this.btn9.Size = new System.Drawing.Size(590, 53);
-            this.btn9.TabIndex = 8;
-            this.btn9.Text = "¿Cómo consultar los datos de las peticiones en SGAR?";
-            this.btn9.UseVisualStyleBackColor = true;
-            this.btn9.Click += new System.EventHandler(this.btn9_Click);
-            // 
-            // btn8
-            // 
-            this.btn8.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btn8.Location = new System.Drawing.Point(0, 371);
-            this.btn8.Name = "btn8";
-            this.btn8.Size = new System.Drawing.Size(590, 53);
-            this.btn8.TabIndex = 7;
-            this.btn8.Text = "¿Cómo se registra una petición en SGAR?";
-            this.btn8.UseVisualStyleBackColor = true;
-            this.btn8.Click += new System.EventHandler(this.btn8_Click);
-            // 
-            // btn7
-            // 
-            this.btn7.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btn7.Location = new System.Drawing.Point(0, 318);
-            this.btn7.Name = "btn7";
-            this.btn7.Size = new System.Drawing.Size(590, 53);
-            this.btn7.TabIndex = 6;
-            this.btn7.Text = "¿Cómo modificar los datos de las ambulancias en SGAR?";
-            this.btn7.UseVisualStyleBackColor = true;
-            this.btn7.Click += new System.EventHandler(this.btn7_Click);
-            // 
-            // btn6
-            // 
-            this.btn6.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btn6.Location = new System.Drawing.Point(0, 265);
-            this.btn6.Name = "btn6";
-            this.btn6.Size = new System.Drawing.Size(590, 53);
-            this.btn6.TabIndex = 5;
-            this.btn6.Text = "¿Cómo modificar los datos de las clientes/conductores/secretarias en SGAR?";
-            this.btn6.UseVisualStyleBackColor = true;
-            this.btn6.Click += new System.EventHandler(this.btn6_Click);
-            // 
-            // btn5
-            // 
-            this.btn5.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btn5.Location = new System.Drawing.Point(0, 212);
-            this.btn5.Name = "btn5";
-            this.btn5.Size = new System.Drawing.Size(590, 53);
-            this.btn5.TabIndex = 4;
-            this.btn5.Text = "¿Cómo consultar los datos de los clientes/conductores/secretarias en SGAR?";
-            this.btn5.UseVisualStyleBackColor = true;
-            this.btn5.Click += new System.EventHandler(this.btn5_Click);
-            // 
-            // btn4
-            // 
-            this.btn4.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btn4.Location = new System.Drawing.Point(0, 159);
-            this.btn4.Name = "btn4";
-            this.btn4.Size = new System.Drawing.Size(590, 53);
-            this.btn4.TabIndex = 3;
-            this.btn4.Text = "¿Cómo consultar los datos de las ambulancias en SGAR?";
-            this.btn4.UseVisualStyleBackColor = true;
-            this.btn4.Click += new System.EventHandler(this.btn4_Click);
-            // 
-            // btn3
-            // 
-            this.btn3.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btn3.Location = new System.Drawing.Point(0, 106);
-            this.btn3.Name = "btn3";
-            this.btn3.Size = new System.Drawing.Size(590, 53);
-            this.btn3.TabIndex = 2;
-            this.btn3.Text = "¿Cómo se registra un cliente en SGAR?";
-            this.btn3.UseVisualStyleBackColor = true;
-            this.btn3.Click += new System.EventHandler(this.btn3_Click);
-            // 
-            // btn2
-            // 
-            this.btn2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btn2.Location = new System.Drawing.Point(0, 53);
-            this.btn2.Name = "btn2";
-            this.btn2.Size = new System.Drawing.Size(590, 53);
-            this.btn2.TabIndex = 1;
-            this.btn2.Text = "¿Cómo se registra una ambulancia en SGAR?";
-            this.btn2.UseVisualStyleBackColor = true;
-            this.btn2.Click += new System.EventHandler(this.btn2_Click);
-            // 
-            // btn1
-            // 
-            this.btn1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btn1.Location = new System.Drawing.Point(0, 0);
-            this.btn1.Name = "btn1";
-            this.btn1.Size = new System.Drawing.Size(590, 53);
-            this.btn1.TabIndex = 0;
-            this.btn1.Text = "¿Cómo se registra un empleado en SGAR?";
-            this.btn1.UseVisualStyleBackColor = true;
-            this.btn1.Click += new System.EventHandler(this.btn1_Click);
-            // 
-            // btn12
-            // 
-            this.btn12.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btn12.Location = new System.Drawing.Point(0, 583);
-            this.btn12.Name = "btn12";
-            this.btn12.Size = new System.Drawing.Size(590, 53);
-            this.btn12.TabIndex = 11;
-            this.btn12.Text = "¿Cómo se registra un conductor en SGAR?";
-            this.btn12.UseVisualStyleBackColor = true;
-            this.btn12.Click += new System.EventHandler(this.btn12_Click);
-            // 
-            // btn13
-            // 
-            this.btn13.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btn13.Location = new System.Drawing.Point(0, 636);
-            this.btn13.Name = "btn13";
-            this.btn13.Size = new System.Drawing.Size(590, 53);
-            this.btn13.TabIndex = 12;
-            this.btn13.Text = "¿Cómo consultar los datos de los conductores en SGAR?";
-            this.btn13.UseVisualStyleBackColor = true;
-            this.btn13.Click += new System.EventHandler(this.btn13_Click);
-            // 
-            // btn14
-            // 
-            this.btn14.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btn14.Location = new System.Drawing.Point(0, 689);
-            this.btn14.Name = "btn14";
-            this.btn14.Size = new System.Drawing.Size(590, 53);
-            this.btn14.TabIndex = 13;
-            this.btn14.Text = "¿Cómo modificar los datos de los conductores en SGAR?";
-            this.btn14.UseVisualStyleBackColor = true;
-            this.btn14.Click += new System.EventHandler(this.btn14_Click);
+            this.btnConfig.Location = new System.Drawing.Point(218, 52);
+            this.btnConfig.Name = "btnConfig";
+            this.btnConfig.Size = new System.Drawing.Size(183, 105);
+            this.btnConfig.TabIndex = 1;
+            this.btnConfig.Text = "Configurar cuenta";
+            this.btnConfig.UseVisualStyleBackColor = true;
             // 
             // Frm_Ayuda
             // 
@@ -279,7 +108,6 @@ namespace Visual
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.panel2.ResumeLayout(false);
-            this.pncontenido.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -289,20 +117,7 @@ namespace Visual
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Panel pncontenido;
-        private System.Windows.Forms.Button btn9;
-        private System.Windows.Forms.Button btn8;
-        private System.Windows.Forms.Button btn7;
-        private System.Windows.Forms.Button btn6;
-        private System.Windows.Forms.Button btn5;
-        private System.Windows.Forms.Button btn4;
-        private System.Windows.Forms.Button btn3;
-        private System.Windows.Forms.Button btn2;
-        private System.Windows.Forms.Button btn1;
-        private System.Windows.Forms.Button btn11;
-        private System.Windows.Forms.Button btn10;
-        private System.Windows.Forms.Button btn14;
-        private System.Windows.Forms.Button btn13;
-        private System.Windows.Forms.Button btn12;
+        private System.Windows.Forms.Button btnAyuda;
+        private System.Windows.Forms.Button btnConfig;
     }
 }
--- Visual/Frm_Ayuda.cs
+++ Visual/Frm_Ayuda.cs
@@ -18,253 +18,29 @@ namespace Visual
         public Frm_Ayuda()
         {
             InitializeComponent();
-            
         }
 
         private void Frm_Ayuda_Load(object sender, EventArgs e)
         {
-            this.pncontenido.BackColor = Color.FromArgb(200, 255, 255, 255);
+            //this.pncontenido.BackColor = Color.FromArgb(200, 255, 255, 255);
+        }
+
+        private void btnAyuda_Click(object sender, EventArgs e)
+        {
             switch (admLogin.TipoUsuario())
             {
                 case "Gerente":
-                    btn8.Visible = false;
-                    btn9.Visible = false;
-                    btn10.Visible = false; 
-                    btn11.Visible = false;
+                    System.Diagnostics.Process.Start("https://docs.google.com/document/d/1i9QP0QT5JUKx3qXi7gQXPC3ds55L7C4nWttKpE6r8Bo/edit?usp=sharing");
                     break;
                 case "Cliente":
-                    btn1.Visible = false;
-                    btn2.Visible = false;
-                    btn3.Visible = false;
-                    btn4.Visible = false; 
-                    btn5.Visible = false;
-                    btn6.Visible = false;
-                    btn7.Visible = false;
-                    btn11.Visible = false;
+                    System.Diagnostics.Process.Start("");
                     break;
                 case "Secretaria":
-                    btn1.Visible = false;
-                    btn3.Visible = false;
-                    btn5.Visible = false;
-                    btn6.Visible = false;
-                    btn8.Visible = false;
-                    btn10.Visible = false;
+                    System.Diagnostics.Process.Start("");
                     break;
                 default:
                     break;
             }
-        }
-
-        private void btn1_Click(object sender, EventArgs e)
-        {
-            string titulo = "\r\n¿Cómo se registra un empleado en SGAR?\r\n";
-            string msj = "\r\n 1- Hacer clic en el botón del menú del cargo que desea agregar (Secretaria/Conductor). \r\n" +
-            "\r\n 2 - Hacer clic en el botón 'Registrar' y llenar los datos correspondientes para tener un registro exitoso.";
-            Frm_Mensaje mensaje = new Frm_Mensaje();
-            mensaje.lblTitulo.Text = titulo;
-            mensaje.txtMensaje.Text = msj;
-            mensaje.ShowDialog();
-        }
-
-        private void btn2_Click(object sender, EventArgs e)
-        {
-            string titulo = "\r\n¿Cómo se registra una ambulancia en SGAR?\r\n";
-            string msj = "\r\n 1- Hacer clic en el botón del menú 'Ambulancia'.\r\n" +
-            "\r\n 2 - Hacer clic en el botón 'Registrar' y llenar los datos correspondientes para tener un registro exitoso.";
-            Frm_Mensaje mensaje = new Frm_Mensaje();
-            mensaje.lblTitulo.Text = titulo;
-            mensaje.txtMensaje.Text = msj;
-            mensaje.ShowDialog();
-        }
-
-        private void btn3_Click(object sender, EventArgs e)
-        {
-            string titulo = "\r\n¿Cómo se registra un cliente en SGAR?\r\n";
-            string msj = "\r\n 1- Hacer clic en el botón del menú 'Cliente'.\r\n" +
-            "\r\n 2- Hacer clic en el botón 'Registrar' y llenar los datos correspondientes para tener un registro exitoso.";
-            Frm_Mensaje mensaje = new Frm_Mensaje();
-            mensaje.lblTitulo.Text = titulo;
-            mensaje.txtMensaje.Text = msj;
-            mensaje.ShowDialog();
-        }
-
-        private void btn4_Click(object sender, EventArgs e)
-        {
-            string titulo = "\r\n¿Cómo consultar los datos de las ambulancias en SGAR?\r\n";
-            string msj = "\r\n 1- Hacer clic en el botón del menú 'Ambulancia'.\r\n" +
-            "\r\n 2- Hacer clic en el botón 'Consultar'.\r\n"+
-            "\r\n 3- Seleccionar entre la opción - placa - y - modelo -, e ingresa el dato según seleccione.\r\n"+ 
-            "\r\n 4- Seleccionar las casillas si se desea filtrar aún más los registros de las ambulancias.\r\n"+
-            "\r\n 5- Hacer clic en 'Buscar'.\r\n";
-            Frm_Mensaje mensaje = new Frm_Mensaje();
-            mensaje.lblTitulo.Text = titulo;
-            mensaje.txtMensaje.Text = msj;
-            mensaje.ShowDialog();
-        }
-
-        private void btn5_Click(object sender, EventArgs e)
-        {
-            string titulo = "\r\n¿Cómo consultar los datos de los clientes/conductores/secretarias en SGAR?\r\n";
-            string msj = "\r\n 1- Hacer clic en el botón del menú 'Cliente' o 'Conductor' o 'Secretaria'.\r\n" +
-            "\r\n 2- Hacer clic en el botón 'Consultar'.\r\n" +
-            "\r\n 3- Seleccionar entre la opción - nombre - y - cédula -, e ingresa el dato según seleccione.\r\n" +
-            "\r\n 4- Seleccionar las casillas si se desea filtrar aún más los registros de clientes/conductores/secretarias.\r\n" +
-            "\r\n 5- Hacer clic en 'Buscar'.\r\n";
-            Frm_Mensaje mensaje = new Frm_Mensaje();
-            mensaje.lblTitulo.Text = titulo;
-            mensaje.txtMensaje.Text = msj;
-            mensaje.ShowDialog();
-        }
-
-        private void btn6_Click(object sender, EventArgs e)
-        {
-            string titulo = "\r\n¿Cómo modificar los datos de las clientes/conductores/secretarias en SGAR?\r\n";
-            string msj = "\r\n 1- Hacer clic en el botón del menú 'Cliente' o 'Conductor' o 'Secretaria'.\r\n" +
-            "\r\n 2- Hacer clic en el botón 'Consultar'.\r\n" +
-            "\r\n 3- Seleccionar un registro de la tabla.\r\n" +
-            "\r\n 4- Hacer clic en el botón 'Modificar'.\r\n" +
-            "\r\n 5- Se abre la ventana de edición, modificar los datos y dar clic en el botón 'Guardar'.\r\n"+
-            "\r\n  \r\n o \r\n " +
-            "\r\n 1- Hacer clic en el botón del menú 'Cliente' o 'Conductor' o 'Secretaria'.\r\n" +
-            "\r\n 2- Hacer clic en el botón 'Modificar'.\r\n" +
-            "\r\n 3- Buscar por cédula.\r\n" +
-            "\r\n 4- Modificar los datos y dar clic en el botón 'Guardar'.\r\n";
-
-            Frm_Mensaje mensaje = new Frm_Mensaje();
-            mensaje.lblTitulo.Text = titulo;
-            mensaje.txtMensaje.Text = msj;
-            mensaje.ShowDialog();
-        }
-
-        private void btn7_Click(object sender, EventArgs e)
-        {
-            string titulo = "\r\n¿Cómo modificar los datos de las ambulancias en SGAR?\r\n";
-            string msj = "\r\n 1- Hacer clic en el botón del menú 'Ambulancia'.\r\n" +
-            "\r\n 2- Hacer clic en el botón 'Consultar'.\r\n" +
-            "\r\n 3- Seleccionar un registro de la tabla.\r\n" +
-            "\r\n 4- Hacer clic en el botón 'Modificar'.\r\n" +
-            "\r\n 5- Se abre la ventana de edición, modificar los datos y dar clic en el botón 'Guardar'.\r\n" +
-            "\r\n  \r\n o \r\n " +
-            "\r\n 1- Hacer clic en el botón del menú 'Ambulancia'.\r\n" +
-            "\r\n 2- Hacer clic en el botón 'Modificar'.\r\n" +
-            "\r\n 3- Buscar por la placa del vehículo.\r\n" +
-            "\r\n 4- Modificar los datos y dar clic en el botón 'Guardar'.\r\n";
-
-            Frm_Mensaje mensaje = new Frm_Mensaje();
-            mensaje.lblTitulo.Text = titulo;
-            mensaje.txtMensaje.Text = msj;
-            mensaje.ShowDialog();
-        }
-
-        private void btn8_Click(object sender, EventArgs e)
-        {
-            string titulo = "\r\n¿Cómo se registra una petición en SGAR?\r\n";
-            string msj = "\r\n 1- Hacer clic en el botón del menú 'Peticiones'.\r\n" +
-            "\r\n 2- Hacer clic en el botón 'Registrar' y llenar los datos correspondientes para tener un registro exitoso.";
-            Frm_Mensaje mensaje = new Frm_Mensaje();
-            mensaje.lblTitulo.Text = titulo;
-            mensaje.txtMensaje.Text = msj;
-            mensaje.ShowDialog();
-        }
-
-        private void btn9_Click(object sender, EventArgs e)
-        {
-            string titulo = "\r\n¿Cómo consultar los datos de las peticiones en SGAR?\r\n";
-            string msj = "\r\n 1- Hacer clic en el botón del menú 'Peticiones'.\r\n" +
-            "\r\n 2- Hacer clic en el botón 'Consultar'.\r\n" +
-            "\r\n 3- Ingresa el punto de destino para filtrar las peticiones.\r\n" +
-            "\r\n 4- Hacer clic en 'Buscar'.\r\n";
-            Frm_Mensaje mensaje = new Frm_Mensaje();
-            mensaje.lblTitulo.Text = titulo;
-            mensaje.txtMensaje.Text = msj;
-            mensaje.ShowDialog();
-        }
-
-        private void btn10_Click(object sender, EventArgs e)
-        {
-            string titulo = "\r\n¿Cómo modificar los datos de las peticiones en SGAR?\r\n";
-            string msj = "\r\n 1- Hacer clic en el botón del menú 'Peticiones'.\r\n" +
-            "\r\n 2- Hacer clic en el botón 'Consultar'.\r\n" +
-            "\r\n 3- Seleccionar un registro de la tabla.\r\n" +
-            "\r\n 4- Hacer clic en el botón 'Modificar'.\r\n" +
-            "\r\n 5- Se abre la ventana de edición, modificar los datos y dar clic en el botón 'Guardar'.\r\n" +
-            "\r\n  \r\n o \r\n " +
-            "\r\n 1- Hacer clic en el botón del menú 'Peticiones'.\r\n" +
-            "\r\n 2- Hacer clic en el botón 'Modificar'.\r\n" +
-            "\r\n 3- Buscar por ?.\r\n" +
-            "\r\n 4- Modificar los datos y dar clic en el botón 'Guardar'.\r\n";
-
-            Frm_Mensaje mensaje = new Frm_Mensaje();
-            mensaje.lblTitulo.Text = titulo;
-            mensaje.txtMensaje.Text = msj;
-            mensaje.ShowDialog();
-        }
-
-        private void btn11_Click(object sender, EventArgs e)
-        {
-            string titulo = "\r\n¿Cómo asignar ambulancia-conductor?\r\n";
-            string msj = "\r\n 1- Hacer clic en el botón del menú 'Peticiones'.\r\n" +
-            "\r\n 2- Hacer clic en el botón 'Asignar'.\r\n" +
-            "\r\n 3- Hacer clic en el botón 'Petición'.\r\n" +
-            "\r\n 4- Seleccionar una petición de la tabla.\r\n" +
-            "\r\n 5- Hacer clic en el botón 'Elegir' y se llenarán los campos con los datos de la petición.\r\n" +
-            "\r\n 6- Hacer clic en el botón 'Conductor' para asignar un conductor a la petición. \r\n " +
-            "\r\n 7- Seleccionar un conductor y dar clic en el botón 'Elegir'. \r\n" +
-            "\r\n 8- Hacer clic en el botón 'Ambulancia' para asignar un ambulancia a la petición.\r\n" +
-            "\r\n 9- Seleccionar una ambulancia y dar clic en el botón 'Elegir'.\r\n" +
-            "\r\n 10- Hacer clic en el botón 'Asignar'.\r\n" +
-            "\r\n 11- Verificar la cantidad de ambulancias requeridas en la petición para finalizar con la asignación.\r\n" +
-            "\r\n 12- Hacer clic en el botón 'Guardar'.\r\n";
-
-            Frm_Mensaje mensaje = new Frm_Mensaje();
-            mensaje.lblTitulo.Text = titulo;
-            mensaje.txtMensaje.Text = msj;
-            mensaje.ShowDialog();
-        }
-
-        private void btn12_Click(object sender, EventArgs e)
-        {
-            string titulo = "\r\n¿Cómo se registra un conductor en SGAR?\r\n";
-            string msj = "\r\n 1- Hacer clic en el botón del menú 'Conductor'. \r\n" +
-            "\r\n 2 - Hacer clic en el botón 'Registrar' y llenar los datos correspondientes para tener un registro exitoso.";
-            Frm_Mensaje mensaje = new Frm_Mensaje();
-            mensaje.lblTitulo.Text = titulo;
-            mensaje.txtMensaje.Text = msj;
-            mensaje.ShowDialog();
-        }
-
-        private void btn13_Click(object sender, EventArgs e)
-        {
-            string titulo = "\r\n¿Cómo consultar los datos de los conductores en SGAR?\r\n";
-            string msj = "\r\n 1- Hacer clic en el botón del menú 'Conductor'.\r\n" +
-            "\r\n 2- Hacer clic en el botón 'Consultar'.\r\n" +
-            "\r\n 3- Seleccionar entre la opción - nombre - y - cédula -, e ingresa el dato según seleccione.\r\n" +
-            "\r\n 4- Seleccionar las casillas si se desea filtrar aún más los registros de conductores.\r\n" +
-            "\r\n 5- Hacer clic en 'Buscar'.\r\n";
-            Frm_Mensaje mensaje = new Frm_Mensaje();
-            mensaje.lblTitulo.Text = titulo;
-            mensaje.txtMensaje.Text = msj;
-            mensaje.ShowDialog();
-        }
-
-        private void btn14_Click(object sender, EventArgs e)
-        {
-            string titulo = "\r\n¿Cómo modificar los datos de los conductores en SGAR?\r\n";
-            string msj = "\r\n 1- Hacer clic en el botón del menú 'Conductor'.\r\n" +
-            "\r\n 2- Hacer clic en el botón 'Consultar'.\r\n" +
-            "\r\n 3- Seleccionar un registro de la tabla.\r\n" +
-            "\r\n 4- Hacer clic en el botón 'Modificar'.\r\n" +
-            "\r\n 5- Se abre la ventana de edición, modificar los datos y dar clic en el botón 'Guardar'.\r\n" +
-            "\r\n  \r\n o \r\n " +
-            "\r\n 1- Hacer clic en el botón del menú 'Conductor'.\r\n" +
-            "\r\n 2- Hacer clic en el botón 'Modificar'.\r\n" +
-            "\r\n 3- Buscar por cédula.\r\n" +
-            "\r\n 4- Modificar los datos y dar clic en el botón 'Guardar'.\r\n";
-
-            Frm_Mensaje mensaje = new Frm_Mensaje();
-            mensaje.lblTitulo.Text = titulo;
-            mensaje.txtMensaje.Text = msj;
-            mensaje.ShowDialog();
         }
     }
 }
